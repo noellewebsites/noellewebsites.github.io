dd9d2440b1ee6204b7d7861ee23f92205fe7a80f
noellewebsites <73456947+noellewebsites@users.noreply.github.com>
2020-11-26 18:38:21 -0600

    JS2

---
+++ JS/OS.js
@@ -0,0 +1,25 @@
+window.onscroll = function() {
+    aboutFunction()
+    cbFunction()
+    pgFunction()
+};
+
+function aboutFunction() {
+    if (document.documentElement.scrollTop > 275) {
+        document.getElementById("A1").className = "r1 col-9 w3-animate-bottom";
+    }
+}
+
+function cbFunction() {
+    if (document.documentElement.scrollTop > 115) {
+        document.getElementById("CB1").className = "ocm-center w3-animate-right";
+    }
+}
+
+function pgFunction() {
+    if (document.documentElement.scrollTop > 1070) {
+        document.getElementById("PG1").className = "row w3-animate-left";
+    }
+}
+
+
