67d02a62cb28595446dd80d60abd7504eb2b782a
noellewebsites <73456947+noellewebsites@users.noreply.github.com>
2020-12-04 10:08:14 -0600

    Update OS.js

--- JS/OS.js
+++ JS/OS.js
@@ -24,7 +24,7 @@ function cvFunction() {
 }
 
 function fqFunction() {
-    if (document.documentElement.scrollTop > 825) {
+    if (document.documentElement.scrollTop > 850) {
         document.getElementById("FAQ1").className = "r1 col-7 w3-animate-right";
     }
 }
