52cf2fa509b8bb575490275a0164895da43627ac
noellewebsites <73456947+noellewebsites@users.noreply.github.com>
2020-12-04 10:05:11 -0600

    Update OS.js

--- JS/OS.js
+++ JS/OS.js
@@ -18,7 +18,7 @@ function cbFunction() {
 }
 
 function cvFunction() {
-    if (document.documentElement.scrollTop > 525) {
+    if (document.documentElement.scrollTop > 500) {
         document.getElementById("CV1").className = "r1 container-o col-11 w3-animate-bottom";
     }
 }
