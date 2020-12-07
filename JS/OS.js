f2aecb795bd60d9880590b11bb53f5ae5129eb3e
noellewebsites <73456947+noellewebsites@users.noreply.github.com>
2020-12-07 10:50:47 -0600

    Update OS.js

--- JS/OS.js
+++ JS/OS.js
@@ -25,13 +25,13 @@ function cvFunction() {
 }
 
 function fqFunction() {
-    if (document.documentElement.scrollTop > 780) {
+    if (document.documentElement.scrollTop > 800) {
         document.getElementById("FAQ1").className = "r1 col-7 w3-animate-bottom";
     }
 }
 
 function adFunction() {
-    if (document.documentElement.scrollTop > 930) {
+    if (document.documentElement.scrollTop > 950) {
         document.getElementById("C1").className = "r1 gmi col-6 w3-animate-left";
     }
 }
