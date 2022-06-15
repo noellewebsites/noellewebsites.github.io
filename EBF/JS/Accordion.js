9b9f286d16cf9472ea4a2c86743b16d744371310
joshbunyan <90576554+newgawebsite@users.noreply.github.com>
2022-06-14 22:18:21 -0500

    Added better FAQ

---
+++ EBF/JS/Accordion.js
@@ -0,0 +1,15 @@
+/* Accordion settings */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active-acc");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
