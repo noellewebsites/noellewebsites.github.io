1466105698d7875a595debccbcf7bc75e8f59d9c
joshbunyan <73456947+noellewebsites@users.noreply.github.com>
2023-01-20 22:31:28 -0600

    Added a test page with buttons

--- EBF/JS/JS.js
+++ EBF/JS/JS.js
@@ -11,3 +11,32 @@ function fixedMenu(x) {
 function openSlideMenu() {
     document.getElementById('myNav').classList.toggle("open");
 }
+
+/* Email Signup Modal */
+var suButton = dcoument.getElementById("SU1");
+var emModal = document.getElementById("EM1");
+var emClose = document.getElementById("EMC");
+var emCancel = document.getElementById("CN1");
+
+/* Open modal on button click */
+suButton.onclick = function() {
+    emModal.style.display = "block";
+    emModal.className = "email-modal animate_animated animate_zoomIn";
+}
+
+/* Close Modal on button click */
+emClose.onclick = function() {
+    emModal.style.display = "none";
+}
+
+/* Close modal with cancel button* */
+emCancel.onclick = function() {
+    emModal.style.display = "none";
+}
+
+//Close modal if user clicks outside
+window.onclick = function(event) {
+    if (event.target == emModal) {
+        emModal.style.display = "none";
+    }
+}
