17d67f8b4f8aeb0f6c33771d459f1520f3170ad9
joshbunyan <73456947+noellewebsites@users.noreply.github.com>
2023-02-05 00:12:45 -0600

    form

--- EBF/JS/JS.js
+++ EBF/JS/JS.js
@@ -12,39 +12,3 @@ function openSlideMenu() {
     document.getElementById('myNav').classList.toggle("open");
 }
 
-/* Email Signup Modal */
-var suButton = dcoument.getElementById("SU1");
-var emModal = document.getElementById("EM1");
-var emClose = document.getElementById("EMC");
-var emCancel = document.getElementById("CN1");
-var emForm = document.getElementById("EF1");
-var opButton = document.getElementById("OP1");
-
-/* Open form on button click */
-opButton.onclick = function() {
-    emForm.style.display = "block";
-    emForm.className = "form-popup col-5 animate_animated animate_zoomIn";
-}
-
-/* Open modal on button click */
-suButton.onclick = function() {
-    emModal.style.display = "block";
-    emModal.className = "email-modal animate_animated animate_zoomIn";
-}
-
-/* Close Modal on button click */
-emClose.onclick = function() {
-    emModal.style.display = "none";
-}
-
-/* Close modal with cancel button* */
-emCancel.onclick = function() {
-    emModal.style.display = "none";
-}
-
-//Close modal if user clicks outside
-window.onclick = function(event) {
-    if (event.target == emModal) {
-        emModal.style.display = "none";
-    }
-}
